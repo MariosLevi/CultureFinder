007a7bfdad05d4b0a3fccc85b43494f984446337
MariosLevi <76508477+MariosLevi@users.noreply.github.com>
2021-03-02 13:00:32 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,712 @@
+var arrays = {
+	"Africa East": {
+	  "pdi": 64,
+	  "idv": 27,
+	  "mas": 41,
+	  "uai": 52,
+	  "ltowvs": 32,
+	  "ivr": 40
+	},
+	"Africa West": {
+	  "pdi": 77,
+	  "idv": 20,
+	  "mas": 46,
+	  "uai": 54,
+	  "ltowvs": 9,
+	  "ivr": 78
+	},
+	"Arab countries": {
+	  "pdi": 80,
+	  "idv": 38,
+	  "mas": 53,
+	  "uai": 68,
+	  "ltowvs": 23,
+	  "ivr": 34
+	},
+	"Argentina": {
+	  "pdi": 49,
+	  "idv": 46,
+	  "mas": 56,
+	  "uai": 86,
+	  "ltowvs": 20,
+	  "ivr": 62
+	},
+	"Australia": {
+	  "pdi": 38,
+	  "idv": 90,
+	  "mas": 61,
+	  "uai": 51,
+	  "ltowvs": 21,
+	  "ivr": 71
+	},
+	"Austria": {
+	  "pdi": 11,
+	  "idv": 55,
+	  "mas": 79,
+	  "uai": 70,
+	  "ltowvs": 60,
+	  "ivr": 63
+	},
+	"Bangladesh": {
+	  "pdi": 80,
+	  "idv": 20,
+	  "mas": 55,
+	  "uai": 60,
+	  "ltowvs": 47,
+	  "ivr": 20
+	},
+	"Belgium": {
+	  "pdi": 65,
+	  "idv": 75,
+	  "mas": 54,
+	  "uai": 94,
+	  "ltowvs": 82,
+	  "ivr": 57
+	},
+	"Brazil": {
+	  "pdi": 69,
+	  "idv": 38,
+	  "mas": 49,
+	  "uai": 76,
+	  "ltowvs": 44,
+	  "ivr": 59
+	},
+	"Bulgaria": {
+	  "pdi": 70,
+	  "idv": 30,
+	  "mas": 40,
+	  "uai": 85,
+	  "ltowvs": 69,
+	  "ivr": 16
+	},
+	"Canada": {
+	  "pdi": 39,
+	  "idv": 80,
+	  "mas": 52,
+	  "uai": 48,
+	  "ltowvs": 36,
+	  "ivr": 68
+	},
+	"Chile": {
+	  "pdi": 63,
+	  "idv": 23,
+	  "mas": 28,
+	  "uai": 86,
+	  "ltowvs": 31,
+	  "ivr": 68
+	},
+	"China": {
+	  "pdi": 80,
+	  "idv": 20,
+	  "mas": 66,
+	  "uai": 30,
+	  "ltowvs": 87,
+	  "ivr": 24
+	},
+	"Colombia": {
+	  "pdi": 67,
+	  "idv": 13,
+	  "mas": 64,
+	  "uai": 80,
+	  "ltowvs": 13,
+	  "ivr": 83
+	},
+	"Croatia": {
+	  "pdi": 73,
+	  "idv": 33,
+	  "mas": 40,
+	  "uai": 80,
+	  "ltowvs": 58,
+	  "ivr": 33
+	},
+	"Czech Rep": {
+	  "pdi": 57,
+	  "idv": 58,
+	  "mas": 57,
+	  "uai": 74,
+	  "ltowvs": 70,
+	  "ivr": 29
+	},
+	"Denmark": {
+	  "pdi": 18,
+	  "idv": 74,
+	  "mas": 16,
+	  "uai": 23,
+	  "ltowvs": 35,
+	  "ivr": 70
+	},
+	"Ecuador": {
+	  "pdi": 78,
+	  "idv": 8,
+	  "mas": 63,
+	  "uai": 67,
+	  "ltowvs": 13,
+	  "ivr": 54
+	},
+	"El Salvador": {
+	  "pdi": 66,
+	  "idv": 19,
+	  "mas": 40,
+	  "uai": 94,
+	  "ltowvs": 20,
+	  "ivr": 89
+	},
+	"Estonia": {
+	  "pdi": 40,
+	  "idv": 60,
+	  "mas": 30,
+	  "uai": 60,
+	  "ltowvs": 82,
+	  "ivr": 16
+	},
+	"Finland": {
+	  "pdi": 33,
+	  "idv": 63,
+	  "mas": 26,
+	  "uai": 59,
+	  "ltowvs": 38,
+	  "ivr": 57
+	},
+	"France": {
+	  "pdi": 68,
+	  "idv": 71,
+	  "mas": 43,
+	  "uai": 86,
+	  "ltowvs": 63,
+	  "ivr": 48
+	},
+	"Germany": {
+	  "pdi": 35,
+	  "idv": 67,
+	  "mas": 66,
+	  "uai": 65,
+	  "ltowvs": 83,
+	  "ivr": 40
+	},
+	"Great Britain": {
+	  "pdi": 35,
+	  "idv": 89,
+	  "mas": 66,
+	  "uai": 35,
+	  "ltowvs": 51,
+	  "ivr": 69
+	},
+	"Greece": {
+	  "pdi": 60,
+	  "idv": 35,
+	  "mas": 57,
+	  "uai": 112,
+	  "ltowvs": 45,
+	  "ivr": 50
+	},
+	"Guatemala": {
+	  "pdi": 95,
+	  "idv": 6,
+	  "mas": 37,
+	  "uai": 101,
+	  "ltowvs": 13,
+	  "ivr": 54
+	},
+	"Hong Kong": {
+	  "pdi": 68,
+	  "idv": 25,
+	  "mas": 57,
+	  "uai": 29,
+	  "ltowvs": 61,
+	  "ivr": 17
+	},
+	"Hungary": {
+	  "pdi": 46,
+	  "idv": 80,
+	  "mas": 88,
+	  "uai": 82,
+	  "ltowvs": 58,
+	  "ivr": 31
+	},
+	"India": {
+	  "pdi": 77,
+	  "idv": 48,
+	  "mas": 56,
+	  "uai": 40,
+	  "ltowvs": 51,
+	  "ivr": 26
+	},
+	"Indonesia": {
+	  "pdi": 78,
+	  "idv": 14,
+	  "mas": 46,
+	  "uai": 48,
+	  "ltowvs": 62,
+	  "ivr": 38
+	},
+	"Iran": {
+	  "pdi": 58,
+	  "idv": 41,
+	  "mas": 43,
+	  "uai": 59,
+	  "ltowvs": 14,
+	  "ivr": 40
+	},
+	"Ireland": {
+	  "pdi": 28,
+	  "idv": 70,
+	  "mas": 68,
+	  "uai": 35,
+	  "ltowvs": 24,
+	  "ivr": 65
+	},
+	"Israel": {
+	  "pdi": 13,
+	  "idv": 54,
+	  "mas": 47,
+	  "uai": 81,
+	  "ltowvs": 38,
+	  "ivr": 50
+	},
+	"Italy": {
+	  "pdi": 50,
+	  "idv": 76,
+	  "mas": 70,
+	  "uai": 75,
+	  "ltowvs": 61,
+	  "ivr": 30
+	},
+	"Jamaica": {
+	  "pdi": 45,
+	  "idv": 39,
+	  "mas": 68,
+	  "uai": 13,
+	  "ltowvs": 13,
+	  "ivr": 80
+	},
+	"Japan": {
+	  "pdi": 54,
+	  "idv": 46,
+	  "mas": 95,
+	  "uai": 92,
+	  "ltowvs": 88,
+	  "ivr": 42
+	},
+	"South Korea": {
+	  "pdi": 60,
+	  "idv": 18,
+	  "mas": 39,
+	  "uai": 85,
+	  "ltowvs": 100,
+	  "ivr": 29
+	},
+	"Latvia": {
+	  "pdi": 44,
+	  "idv": 70,
+	  "mas": 9,
+	  "uai": 63,
+	  "ltowvs": 69,
+	  "ivr": 13
+	},
+	"Lithuania": {
+	  "pdi": 42,
+	  "idv": 60,
+	  "mas": 19,
+	  "uai": 65,
+	  "ltowvs": 82,
+	  "ivr": 16
+	},
+	"Luxembourg": {
+	  "pdi": 40,
+	  "idv": 60,
+	  "mas": 50,
+	  "uai": 70,
+	  "ltowvs": 64,
+	  "ivr": 56
+	},
+	"Malaysia": {
+	  "pdi": 104,
+	  "idv": 26,
+	  "mas": 50,
+	  "uai": 36,
+	  "ltowvs": 41,
+	  "ivr": 57
+	},
+	"Malta": {
+	  "pdi": 56,
+	  "idv": 59,
+	  "mas": 47,
+	  "uai": 96,
+	  "ltowvs": 47,
+	  "ivr": 66
+	},
+	"Mexico": {
+	  "pdi": 81,
+	  "idv": 30,
+	  "mas": 69,
+	  "uai": 82,
+	  "ltowvs": 24,
+	  "ivr": 97
+	},
+	"Morocco": {
+	  "pdi": 70,
+	  "idv": 46,
+	  "mas": 53,
+	  "uai": 68,
+	  "ltowvs": 14,
+	  "ivr": 25
+	},
+	"Netherlands": {
+	  "pdi": 38,
+	  "idv": 80,
+	  "mas": 14,
+	  "uai": 53,
+	  "ltowvs": 67,
+	  "ivr": 68
+	},
+	"New Zealand": {
+	  "pdi": 22,
+	  "idv": 79,
+	  "mas": 58,
+	  "uai": 49,
+	  "ltowvs": 33,
+	  "ivr": 75
+	},
+	"Norway": {
+	  "pdi": 31,
+	  "idv": 69,
+	  "mas": 8,
+	  "uai": 50,
+	  "ltowvs": 35,
+	  "ivr": 55
+	},
+	"Pakistan": {
+	  "pdi": 55,
+	  "idv": 14,
+	  "mas": 50,
+	  "uai": 70,
+	  "ltowvs": 50,
+	  "ivr": 0
+	},
+	"Panama": {
+	  "pdi": 95,
+	  "idv": 11,
+	  "mas": 44,
+	  "uai": 86,
+	  "ltowvs": 13,
+	  "ivr": 54
+	},
+	"Peru": {
+	  "pdi": 64,
+	  "idv": 16,
+	  "mas": 42,
+	  "uai": 87,
+	  "ltowvs": 25,
+	  "ivr": 46
+	},
+	"Philippines": {
+	  "pdi": 94,
+	  "idv": 32,
+	  "mas": 64,
+	  "uai": 44,
+	  "ltowvs": 27,
+	  "ivr": 42
+	},
+	"Poland": {
+	  "pdi": 68,
+	  "idv": 60,
+	  "mas": 64,
+	  "uai": 93,
+	  "ltowvs": 38,
+	  "ivr": 29
+	},
+	"Portugal": {
+	  "pdi": 63,
+	  "idv": 27,
+	  "mas": 31,
+	  "uai": 104,
+	  "ltowvs": 28,
+	  "ivr": 33
+	},
+	"Romania": {
+	  "pdi": 90,
+	  "idv": 30,
+	  "mas": 42,
+	  "uai": 90,
+	  "ltowvs": 52,
+	  "ivr": 20
+	},
+	"Russia": {
+	  "pdi": 93,
+	  "idv": 39,
+	  "mas": 36,
+	  "uai": 95,
+	  "ltowvs": 81,
+	  "ivr": 20
+	},
+	"Serbia": {
+	  "pdi": 86,
+	  "idv": 25,
+	  "mas": 43,
+	  "uai": 92,
+	  "ltowvs": 52,
+	  "ivr": 28
+	},
+	"Singapore": {
+	  "pdi": 74,
+	  "idv": 20,
+	  "mas": 48,
+	  "uai": 8,
+	  "ltowvs": 72,
+	  "ivr": 46
+	},
+	"Slovakia": {
+	  "pdi": 104,
+	  "idv": 52,
+	  "mas": 85,
+	  "uai": 51,
+	  "ltowvs": 77,
+	  "ivr": 28
+	},
+	"Slovenia": {
+	  "pdi": 71,
+	  "idv": 27,
+	  "mas": 19,
+	  "uai": 88,
+	  "ltowvs": 49,
+	  "ivr": 48
+	},
+	"Spain": {
+	  "pdi": 57,
+	  "idv": 51,
+	  "mas": 42,
+	  "uai": 86,
+	  "ltowvs": 48,
+	  "ivr": 44
+	},
+	"Sweden": {
+	  "pdi": 31,
+	  "idv": 71,
+	  "mas": 5,
+	  "uai": 29,
+	  "ltowvs": 53,
+	  "ivr": 78
+	},
+	"Switzerland": {
+	  "pdi": 34,
+	  "idv": 68,
+	  "mas": 70,
+	  "uai": 58,
+	  "ltowvs": 74,
+	  "ivr": 66
+	},
+	"Taiwan": {
+	  "pdi": 58,
+	  "idv": 17,
+	  "mas": 45,
+	  "uai": 69,
+	  "ltowvs": 93,
+	  "ivr": 49
+	},
+	"Thailand": {
+	  "pdi": 64,
+	  "idv": 20,
+	  "mas": 34,
+	  "uai": 64,
+	  "ltowvs": 32,
+	  "ivr": 45
+	},
+	"Trinidad and Tobago": {
+	  "pdi": 47,
+	  "idv": 16,
+	  "mas": 58,
+	  "uai": 55,
+	  "ltowvs": 13,
+	  "ivr": 80
+	},
+	"Turkey": {
+	  "pdi": 66,
+	  "idv": 37,
+	  "mas": 45,
+	  "uai": 85,
+	  "ltowvs": 46,
+	  "ivr": 49
+	},
+	"U.S.A.": {
+	  "pdi": 40,
+	  "idv": 91,
+	  "mas": 62,
+	  "uai": 46,
+	  "ltowvs": 26,
+	  "ivr": 68
+	},
+	"Uruguay": {
+	  "pdi": 61,
+	  "idv": 36,
+	  "mas": 38,
+	  "uai": 100,
+	  "ltowvs": 26,
+	  "ivr": 53
+	},
+	"Venezuela": {
+	  "pdi": 81,
+	  "idv": 12,
+	  "mas": 73,
+	  "uai": 76,
+	  "ltowvs": 16,
+	  "ivr": 100
+	},
+	"Vietnam": {
+	  "pdi": 70,
+	  "idv": 20,
+	  "mas": 40,
+	  "uai": 30,
+	  "ltowvs": 57,
+	  "ivr": 35
+	}
+  };
+
+
+var HTML_OPTIONS="";
+
+for(var i=0; i<Object.keys(arrays).length; i++){
+	HTML_OPTIONS +="<option>"+Object.keys(arrays)[i]+"</option>";
+}
+	  
+function populateSelect(){
+
+	document.getElementById('fatherCountry').innerHTML=HTML_OPTIONS;
+	document.getElementById('motherCountry').innerHTML=HTML_OPTIONS;
+	document.getElementById('your_country_0').innerHTML=HTML_OPTIONS;
+}
+
+populateSelect()
+
+var rows = 0;
+function addRow(event){
+	if(event)  event.preventDefault();
+	rows++
+	var html = 
+			`<div id=new_row_${rows}> <select id="your_country_${rows}" class="form-control">${HTML_OPTIONS}</select>
+			Years lived: <input id = "year_${rows}" type="number"><br></br></div>
+				`;
+	document.getElementById('you_info').insertAdjacentHTML('beforeend',html);
+
+}
+
+function removeRow(event){
+	if(event)  event.preventDefault();
+	var html =
+			`<select class="form-control">${HTML_OPTIONS}</select>
+			Years lived: <input id="formy" type="number" value="years1"><br></br>
+				`;
+	var lastone = 'new_row_' + rows;
+	document.getElementById(lastone).remove();
+	rows--;
+}
+
+//delete the "var", it's not needed
+var dict = [];
+function results(){
+	var dad = document.getElementById('fatherCountry').value;
+	var mom = document.getElementById('motherCountry').value;
+	var parents_pdi = (arrays[dad].pdi + arrays[mom].pdi)/2
+	var parents_idv = (arrays[dad].idv + arrays[mom].idv)/2
+	var parents_mas = (arrays[dad].mas + arrays[mom].mas)/2
+	var parents_uai = (arrays[dad].uai + arrays[mom].uai)/2
+	var parents_ltowvs = (arrays[dad].ltowvs + arrays[mom].ltowvs)/2
+	var parents_ivr = (arrays[dad].ivr + arrays[mom].ivr)/2
+	//parents averaged out
+	//console.log(parents_pdi,parents_idv,parents_mas,parents_uai,parents_ltowvs,parents_ivr)
+	var age = 0;
+	for(var i=0; i<rows+1; i++){
+		age = age + parseInt(document.getElementById('year_' + i).value)
+		
+		//console.log(document.getElementById('your_country_'+i).value)
+	}
+	//console.log('total age is ' + age)
+	weighted_pdi = 0
+	weighted_idv = 0
+	weighted_mas = 0
+	weighted_uai = 0
+	weighted_ltowvs = 0
+	weighted_ivr = 0
+	for(var i=0; i<rows+1; i++){
+		countries = rows + 1
+		weightedAge = parseInt(document.getElementById('year_' + i).value)/age
+		currentCountry = document.getElementById('your_country_'+i).value
+		weighted_pdi = weightedAge*arrays[currentCountry].pdi + weighted_pdi
+		weighted_idv = weightedAge*arrays[currentCountry].idv + weighted_idv
+		weighted_mas = weightedAge*arrays[currentCountry].mas + weighted_mas
+		weighted_uai = weightedAge*arrays[currentCountry].uai + weighted_uai
+		weighted_ltowvs = weightedAge*arrays[currentCountry].ltowvs + weighted_ltowvs
+		weighted_ivr = weightedAge*arrays[currentCountry].ivr + weighted_ivr
+	
+	}
+	final_pdi = (weighted_pdi+parents_pdi)/2
+	final_idv = (weighted_idv+parents_idv)/2
+	final_mas = (weighted_mas+parents_mas)/2
+	final_uai = (weighted_uai+parents_uai)/2
+	final_ltowvs = (weighted_ltowvs+parents_ltowvs)/2
+	final_ivr = (weighted_ivr+parents_ivr)/2
+	//console.log(final_pdi,final_idv,final_mas,final_uai,final_ltowvs,final_ivr)
+
+	for(var i=0; i<Object.keys(arrays).length; i++){
+		
+		
+		currentCountry_pdi = arrays[Object.keys(arrays)[i]].pdi
+		currentCountry_idv = arrays[Object.keys(arrays)[i]].idv
+		currentCountry_mas = arrays[Object.keys(arrays)[i]].mas
+		currentCountry_uai = arrays[Object.keys(arrays)[i]].uai
+		currentCountry_ltowvs = arrays[Object.keys(arrays)[i]].ltowvs
+		currentCountry_ivr = arrays[Object.keys(arrays)[i]].ivr
+
+		//the juice
+		euclidianNumber = Math.sqrt(Math.pow(currentCountry_pdi-final_pdi,2)+Math.pow(currentCountry_idv-final_idv,2)+
+		Math.pow(currentCountry_mas-final_mas,2)+Math.pow(currentCountry_uai-final_uai,2)+Math.pow(currentCountry_ltowvs-final_ltowvs,2)
+		+Math.pow(currentCountry_ivr-final_ivr,2))
+
+		dict[Object.keys(arrays)[i]] = euclidianNumber
+		// dict.push({
+		// 	key:   Object.keys(arrays)[i],
+		// 	value: euclidianNumber
+		// });
+		
+	}
+
+	// Create items array
+var items = Object.keys(dict).map(function(key) {
+	return [key, dict[key]];
+	
+  });
+  
+  // Sort the array based on the second element
+  items.sort((a,b) => {
+		return b[1]- a[1]
+	});
+ 
+  // Create a new array with only the first 5 items
+  var dissimilar =[];
+ for(var i=0; i<3;i++){ 
+	dissimilar.push(items.slice(0, 3)[i][0]);
+ }
+
+ items.sort((a,b) => {
+	return a[1]- b[1]
+});
+var similar =[];
+ for(var i=0; i<3;i++){ 
+	similar.push(items.slice(0, 3)[i][0]);
+   }
+   console.log(similar[0])
+   document.getElementById('similar').innerHTML += similar[0] + ", " + similar[1] + ", and " + similar[2]
+   document.getElementById('dissimilar').innerHTML += dissimilar[0] + ", " + dissimilar[1] + ", and " + dissimilar[2]
+}
+
+
+//find weighted average for You, without parents
+	//find years in each value
+	//divide each index by sum, find what country corresponds to that weighted average
+//Average out above, parental influence
+//do the least squares approx to make an array of numbers
+//sort by the smallest to largest number
+//display smallest 3, and largest 3
+
+
+//for countries in You, do the math here
+
